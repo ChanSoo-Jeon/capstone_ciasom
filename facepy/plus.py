f2582fd00fc0067879614489a2b616a2c684760f
Hosung00 <144683490+Hosung00@users.noreply.github.com>
2023-11-18 13:36:45 +0900

    OpenCV를 활용한 face mosaic
    실시간 얼굴 모자이크, 동영상 파일 모자이크, 화면 녹화 및 캡쳐 기능 구현

---
+++ facepy/plus.py
@@ -0,0 +1,74 @@
+import cv2
+import datetime
+from PIL import ImageFont, ImageDraw, Image
+import numpy as np
+
+# 실시간, 모자이크, 화면 녹화, 캡쳐, 글자삽입, 시간표시
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+font = ImageFont.truetype("fonts/SCDream6.otf", 20)
+is_record = False
+
+face_cascade = cv2.CascadeClassifier("haarcascade/haarcascade_frontalface_default.xml")
+v = 20
+
+while True:
+    now = datetime.datetime.now()
+    nowDatetime = now.strftime("%Y-%m-%d %H:%M:%S")
+    nowDatetime_path = now.strftime("%Y-%m-%d %H_%M_%S")
+
+    ret, frame = capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray, scaleFactor=1.5, minNeighbors=3, minSize=(20, 20)
+    )
+
+    cv2.rectangle(img=frame, pt1=(10, 15), pt2=(340, 35), color=(0, 0, 0), thickness=-1)
+
+    frame = Image.fromarray(frame)
+    draw = ImageDraw.Draw(frame)
+    draw.text(xy=(10, 15), text="얼굴 인식" + nowDatetime, font=font, fill=(255, 255, 255))
+    frame = np.array(frame)
+
+    key = cv2.waitKey(30)
+
+    if len(faces):
+        for x, y, w, h in faces:
+            roi_gray = gray[y : y + h, x : x + w]
+            roi_color = frame[y : y + h, x : x + w]
+
+            roi = cv2.resize(roi_color, (w // v, h // v))
+            roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_AREA)
+            frame[y : y + h, x : x + w] = roi
+
+    if key == ord("r") and is_record == False:
+        is_record = True
+
+        video = cv2.VideoWriter(
+            "video" + nowDatetime_path + ".avi",
+            fourcc,
+            10,
+            (frame.shape[1], frame.shape[0]),
+        )
+    elif key == ord("r") and is_record == True:
+        is_record = False
+        video.release()
+    elif key == ord("c"):
+        cv2.imwrite("capture " + nowDatetime_path + ".png", frame)
+    elif key == ord("q"):
+        break
+    if is_record == True:
+        video.write(frame)
+        cv2.circle(
+            img=frame, center=(620, 15), radius=5, color=(0, 0, 255), thickness=-1
+        )
+
+    cv2.imshow("test", frame)
+
+
+capture.release()
+cv2.destroyAllWindows()
